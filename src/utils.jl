773adc666cf0f2c423ac6fec7accd0f05543289d
Josselin Morvan <morvan.josselin@gmail.com>
2026-02-12 16:10:01 +0100

    moved function definepath!() from towsty to towstytemplate

--- src/utils.jl
+++ src/utils.jl
@@ -54,9 +54,13 @@ function newproject(dir::String="project"; template::String="jj", changedir::Boo
   # move to the directory if relevant
   changedir && cd(dir)
 
-  # Update TowSty paths if the module is loaded
+  #== Update TowSty paths if the module is loaded
   if changedir && isdefined(Main, :TowSty) && isdefined(Main.TowSty, :definepaths!)
     Base.invokelatest(Main.TowSty.definepaths!)
+  end==#
+
+  if changedir && isdefined(Main, :TowSty)
+    definepath!()
   end
 
   # display information as adequate
@@ -87,6 +91,23 @@ function mergefolders(src, dst)
         end
     end
 end
+
+
+"""
+    definepaths!()
+
+"""
+function definepaths!()
+  global PROJECT_PATH = pwd()
+  global ASSETS_PATH = joinpath(PROJECT_PATH, "assets")
+  global TEMP_PATH = joinpath(PROJECT_PATH, "temp")
+  global TEMPLATES_PATH = joinpath(PROJECT_PATH, "templates")
+  global DATA_PATH = joinpath(PROJECT_PATH, "content", "workspace.json")
+  global BIB_PATH = joinpath(TEMP_PATH, "bib.bib")  # Bibliography files are created on the fly
+  global CSL_PATH = joinpath(ASSETS_PATH, "static/csl/style.csl")  # fichier csl / citations bibliographiques
+  return nothing
+end
+
 
 """
     templates()
