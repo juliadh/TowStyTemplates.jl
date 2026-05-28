8e953a4dbb44794bbc68be4beef9df723fb0dbc8
Josselin Morvan <morvan.josselin@gmail.com>
2026-05-28 14:18:43 +0200

    removed definepaths call from newproject()

--- src/utils.jl
+++ src/utils.jl
@@ -54,17 +54,6 @@ function newproject(dir::String="project"; template::String="jj", changedir::Boo
   # move to the directory if relevant
   changedir && cd(dir)
 
-  # Update TowSty paths if the module is loaded
-  if changedir
-    for (pkgid, mod) in Base.loaded_modules
-      if pkgid.name == "TowSty" && isdefined(mod, :definepaths)
-        Base.invokelatest(mod.definepaths)
-        @info "Project is now at $(PROJECT_PATH)"
-        break
-      end
-    end
-  end
-
   # display information as adequate
   verbose && begin
     print("✓ project folder generated at \"$(dir)\"")
